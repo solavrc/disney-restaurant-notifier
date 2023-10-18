d76a7b8c0c24dbb6c190e2e2aaa15e46662d1977
solavrc <cadres-08-matting@icloud.com>
2023-10-18 11:33:34 +0900

    Initial commit

---
+++ test/disney-restaurant-notifier.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DisneyRestaurantNotifier from '../lib/disney-restaurant-notifier-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/disney-restaurant-notifier-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DisneyRestaurantNotifier.DisneyRestaurantNotifierStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
